4419221a5ad98c7eea1b239e691bbf5466eac1f2
m-iftikhar <iftikharrj2001@gmail.com>
2026-01-29 12:35:18 +0500

    db connections done

--- server/app.ts
+++ server/app.ts
@@ -1,18 +1,21 @@
-import express from 'express';
+require("dotenv").config();
+import express,{ Request,Response,NextFunction } from 'express';
 export const app = express();
 import cookieParser from 'cookie-parser';
-import{ Request,Response,NextFunction } from 'express';
-require('dotenv').config();
 import cors from 'cors';
 
+
+
+
+app.use(cors({
+    origin: process.env.ORIGIN,
+}));
 
 
 app.use(express.json({ limit: '50mb' }));
 
 app.use(cookieParser());
-app.use(cors({
-    origin: process.env.ORIGIN,
-}));
+
 
 // test route
 app.get("/test", (req:Request, res:Response, next:NextFunction) => {
@@ -24,12 +27,8 @@ app.get("/test", (req:Request, res:Response, next:NextFunction) => {
 
 
 
-// unknown route.
-app.all("*", (req: Request, res: Response, next: NextFunction) => {
-  const err = new Error(`Route ${req.originalUrl} not found`) as any;
-  err.statusCode = 404;
-  next(err);
-});
+
+
 
 
 
--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,9 @@
 import { app } from "./app";
 require('dotenv').config();
+import connectDB from "./utils/db";
 
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
+    connectDB();
 });
