f9fde17b4814a529d9026a6395303133554162a2
m-iftikhar <iftikharrj2001@gmail.com>
2026-01-31 17:05:20 +0500

    login user done

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -7,6 +7,11 @@ import ErrorHandler from "../utils/ErrorHandler";
 import { CatchAsyncError } from "../middleware/catchAsyncErrors";
 import sendMail from "../utils/sendEmail";
 import { Secret } from "jsonwebtoken";
+import {
+  accessTokenOptions,
+  refreshTokenOptions,
+  sendToken,
+} from "../utils/jwt";
 
 
 interface IRegistrationBody {
@@ -137,3 +142,38 @@ export const activateUser = CatchAsyncError(
   }
 );
 
+
+// login user
+
+interface ILoginRequest {
+  email: string;
+  password: string;
+}
+
+export const loginUser = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    console.log("loginuser");
+    try {
+      const { email, password } = req.body as ILoginRequest;
+
+      if (!email || !password) {
+        return next(new ErrorHandler("Please enter email and password", 400));
+      }
+
+      const user = await userModel.findOne({ email }).select("+password");
+
+      if (!user) {
+        return next(new ErrorHandler("Invalid email or password", 400));
+      }
+
+      const isPasswordMatch = await user.comparePassword(password);
+      if (!isPasswordMatch) {
+        return next(new ErrorHandler("Invalid email or password", 400));
+      }
+      console.log("dhhdh", user);
+      sendToken(user, 200, res);
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,9 +1,10 @@
 import express from "express";
 const userRouter = express.Router();
-import { registrationUser,activateUser } from "../controllers/user.controller";
+import { registrationUser,activateUser, loginUser } from "../controllers/user.controller";
 
 userRouter.post("/registration", registrationUser);
 userRouter.post("/activate",activateUser);
+userRouter.post("/login",loginUser);
 
 
 export default userRouter;
