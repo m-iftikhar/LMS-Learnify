6ae1aec37068905caf5c7f722a7ffd1aca449689
m-iftikhar <iftikharrj2001@gmail.com>
2026-05-01 22:03:33 +0500

    analytics done

--- server/app.ts
+++ server/app.ts
@@ -8,6 +8,7 @@ import userRouter from "./routes/user.route";
 import courseRouter from "./routes/course.route";
 import orderRouter from "./routes/order.route";
 import notificationRoute from "./routes/notification.route";
+import analyticsRouter from "./routes/analytics.route";
 app.use(
   cors({
     origin: process.env.ORIGIN,
@@ -18,7 +19,7 @@ app.use(express.json({ limit: "50mb" }));
 
 app.use(cookieParser());
 
-app.use("/api/v1", userRouter, courseRouter, orderRouter, notificationRoute);
+app.use("/api/v1", userRouter, courseRouter, orderRouter, notificationRoute, analyticsRouter);
 
 // test route
 app.get("/test", (req: Request, res: Response, next: NextFunction) => {
