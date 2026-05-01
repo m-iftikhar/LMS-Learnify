88624a89fe2213f7eb2df68971aee6cfcea9766b
m-iftikhar <iftikharrj2001@gmail.com>
2026-05-01 21:15:38 +0500

    orders done

--- server/app.ts
+++ server/app.ts
@@ -1,41 +1,31 @@
 require("dotenv").config();
-import express,{ Request,Response,NextFunction } from 'express';
+import express, { Request, Response, NextFunction } from "express";
 export const app = express();
-import cookieParser from 'cookie-parser';
-import {ErrorMiddleware} from './middleware/error';
-import cors from 'cors';
-import userRouter from './routes/user.route';
-import courseRouter from "./routes/course.route"
-
-app.use(cors({
+import cookieParser from "cookie-parser";
+import { ErrorMiddleware } from "./middleware/error";
+import cors from "cors";
+import userRouter from "./routes/user.route";
+import courseRouter from "./routes/course.route";
+import orderRouter from "./routes/order.route";
+import notificationRoute from "./routes/notification.route";
+app.use(
+  cors({
     origin: process.env.ORIGIN,
-}));
+  }),
+);
 
-
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json({ limit: "50mb" }));
 
 app.use(cookieParser());
 
-app.use(
-  "/api/v1",
-  userRouter,
-  courseRouter
-);
-
+app.use("/api/v1", userRouter, courseRouter, orderRouter, notificationRoute);
 
 // test route
-app.get("/test", (req:Request, res:Response, next:NextFunction) => {
-    res.status(200).json({
-        message: "Server is running!",
-        success: true
-    });
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({
+    message: "Server is running!",
+    success: true,
+  });
 });
 
 app.use(ErrorMiddleware);
-
-
-
-
-
-
-
---
+++ server/routes/order.route.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import { createOrder, getAllOrders} from "../controllers/order.controller";
+import { isAutheticated,authorizeRoles } from "../middleware/auth";
+
+const orderRouter = express.Router();
+
+orderRouter.post("/create-order", isAutheticated, createOrder);
+orderRouter.get(
+  "/get-orders",
+  isAutheticated,
+  authorizeRoles("admin"),
+  getAllOrders
+);
+
+export default orderRouter;
